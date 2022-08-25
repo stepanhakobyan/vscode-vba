1b3dd066536482eff9b4a0a80b96c12d9b05a02b
Lukas Neubert <serkonda7@pm.me>
2022-08-25 16:58:49 +0200

    initial progress on vba.extract

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
-import * as vscode from 'vscode';
+import * as fs from 'fs'
+import * as vscode from 'vscode'
+import * as yauzl from 'yauzl'
 
 export function activate(context: vscode.ExtensionContext) {
 	// set context keys
@@ -6,9 +8,40 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// register commands
 	const commands = {
-		'vba.extract': () => {
+		'vba.extract': (uri: vscode.Uri) => {
+			yauzl.open(uri.path, { lazyEntries: true }, (err, zipfile) => {
+				if (err) {
+					vscode.window.showErrorMessage(err.message)
+					throw err
+				}
+				zipfile.readEntry()
+				zipfile.on('entry', (entry: yauzl.Entry) => {
+					if (entry.fileName == 'xl/vbaProject.bin') {
+						zipfile.openReadStream(entry, (err, stream) => {
+							if (err) {
+								vscode.window.showErrorMessage(err.message)
+								throw err
+							}
+							// TODO read vbaProject.bin
+							const chunks: Buffer[] = []
+							stream.on('data', (chunk) => {
+								chunks.push(chunk)
+							});
+							stream.once('end', () => {
+								// TODO read and convert ole to text
+								const bufstr = Buffer.concat(chunks).toString()
+								// TODO split text per module and output to files
+							})
+						})
+						zipfile.close()
+					} else {
+						zipfile.readEntry()
+					}
+				})
+			})
 		},
-		'vba.write': () => {
+		'vba.write': (uri: vscode.Uri) => {
+			// TODO
 		},
 	}
 	Object.entries(commands).forEach(([name, callback]) => {
