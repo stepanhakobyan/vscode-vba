233b53f5d5d3c35139fe7eb3d28f6e60ff6365d4
Lukas Neubert <serkonda7@pm.me>
2022-08-25 14:24:32 +0200

    register commands
    - they are available as context menu on office files (for now: docm, xlsm)

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,19 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+	// set context keys
+	vscode.commands.executeCommand('setContext', 'vba.fileExts', ['.docm', '.xlsm'])
+
+	// register commands
+	const commands = {
+		'vba.extract': () => {
+		},
+		'vba.write': () => {
+		},
+	}
+	Object.entries(commands).forEach(([name, callback]) => {
+		vscode.commands.registerCommand(name, callback)
+	})
+
 	console.log('vscode-vba activated.')
 }
